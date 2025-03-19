f7ec2ddd48d7a0ba8b182fad7a5c4108b5cb830a
gothedistance <michitaka@quality-start.in>
2025-03-20 01:15:49 +0900

    JSONデータのサンプル追加

---
+++ lib/data.dart
@@ -0,0 +1,23 @@
+const dummyData = {
+  "version": "令和６年",
+  "count": 1,
+  "question": "マーケティングオートメーションの定義で適切なものは何か？",
+  "options": [
+    {"key": "ア", "value": "マーケティング活動を自動化するためのソフトウェア"},
+    {"key": "イ", "value": "マーケティング活動を自動化するためのソフトウェア"},
+    {"key": "ウ", "value": "マーケティング活動を自動化するためのソフトウェア"},
+    {"key": "エ", "value": "マーケティング活動を自動化するためのソフトウェア"},
+  ],
+  "category": "ストラテジ系 > 経営戦略マネジメント > マーケティング",
+  "answer": "ア",
+  "description": "マーケティングオートメーション（MA）は、マーケティング活動を自動化するためのソフトウェアです。",
+};
+
+//Map<String, Object> -> Question
+/*
+Question toQuestion(Map<String, Object> data) {
+  return Question(
+    number: data['number'] as int,
+    title: data['title'] as String,
+  );
+}*/
---
+++ lib/question.dart
@@ -0,0 +1,13 @@
+class Question {
+  final int number;
+  final String title;
+
+  Question({required this.number, required this.title});
+}
+
+class Option {
+  final String key;
+  final String value;
+
+  Option({required this.key, required this.value});
+}
