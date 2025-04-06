5dbad70b89721382b922e19e9ce9e85a57780cc6
ArataUchida <158299930+ArataUchida@users.noreply.github.com>
2025-04-06 17:50:11 +0900

    FutureBuild追加

--- lib/feature/quiz/quiz_screen.dart
+++ lib/feature/quiz/quiz_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:it_passport_training_app/data.dart';
+import 'package:it_passport_training_app/feature/core/quiz.dart';
 import 'package:it_passport_training_app/feature/quiz/components/answer_button.dart';
 import 'package:it_passport_training_app/feature/quiz/components/answer_widget.dart';
 import 'package:it_passport_training_app/feature/quiz/components/choices_widget.dart';
@@ -30,70 +31,82 @@ class QuizScreenState extends State<QuizScreen> {
 
   @override
   Widget build(BuildContext context) {
-    final quizData = quizList[current - 1];
+    //final quizData = quizList[current - 1];
 
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          centerTitle: false,
-          title: const Text('ITパスポート2020', style: TextStyle(color: Colors.black)),
-          backgroundColor: Colors.orange,
-        ),
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              children: [
-                // 問題数
-                QuestionNumberWidget(number: quizData.id),
-                // 問題
-                QuestionWidget(questionText: quizData.question),
-                // 問題の選択肢
-                ChoicesWidget(
-                  choiceQuestions: quizData.choices,
-                  selected: selectAnswer,
-                  onSelected:
-                      (answer) => setState(() {
-                        selectAnswer = answer + 1;
-                      }),
+    return FutureBuilder<Quiz>(
+      future: getQuizData(current),
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Text(snapshot.error.toString());
+        }
+        if (snapshot.hasData) {
+          final quizData = snapshot.data!;
+          return MaterialApp(
+            home: Scaffold(
+              appBar: AppBar(
+                centerTitle: false,
+                title: const Text('ITパスポート2020', style: TextStyle(color: Colors.black)),
+                backgroundColor: Colors.orange,
+              ),
+              body: SingleChildScrollView(
+                child: Padding(
+                  padding: const EdgeInsets.all(16.0),
+                  child: Column(
+                    children: [
+                      // 問題数
+                      QuestionNumberWidget(number: quizData.id),
+                      // 問題
+                      QuestionWidget(questionText: quizData.question),
+                      // 問題の選択肢
+                      ChoicesWidget(
+                        choiceQuestions: quizData.choices,
+                        selected: selectAnswer,
+                        onSelected:
+                            (answer) => setState(() {
+                              selectAnswer = answer + 1;
+                            }),
+                      ),
+                      AnswerButton(
+                        onPressed:
+                            selectAnswer != 0 && finalAnswer == 0
+                                ? () async {
+                                  setState(() {
+                                    finalAnswer = selectAnswer;
+                                  });
+                                  finalAnswer == quizData.answer
+                                      ? showAnswerDialog(context, "正解！", "○", Colors.green)
+                                      : showAnswerDialog(context, "残念‥", "×", Colors.red);
+                                }
+                                : null,
+                      ),
+                      //分類・正解・解説
+                      if (finalAnswer > 0) ...[
+                        ClassificationWidget(classification: quizData.classification),
+                        AnswerWidget(answer: quizData.answer),
+                        DescriptionWidget(description: quizData.descriptions),
+                      ],
+                    ],
+                  ),
                 ),
-                AnswerButton(
-                  onPressed:
-                      selectAnswer != 0 && finalAnswer == 0
-                          ? () async {
-                            setState(() {
-                              finalAnswer = selectAnswer;
-                            });
-                            finalAnswer == quizData.answer
-                                ? showAnswerDialog(context, "正解！", "○", Colors.green)
-                                : showAnswerDialog(context, "残念‥", "×", Colors.red);
-                          }
-                          : null,
-                ),
-                //分類・正解・解説
-                if (finalAnswer > 0) ...[
-                  ClassificationWidget(classification: quizData.classification),
-                  AnswerWidget(answer: quizData.answer),
-                  DescriptionWidget(description: quizData.descriptions),
-                ],
-              ],
+              ),
+              floatingActionButton:
+                  finalAnswer > 0
+                      ? FloatingActionButton(
+                        onPressed: () {
+                          setState(() {
+                            current++;
+                            selectAnswer = 0;
+                            finalAnswer = 0;
+                          });
+                        },
+                        child: const Icon(Icons.arrow_forward),
+                      )
+                      : null,
             ),
-          ),
-        ),
-        floatingActionButton:
-            finalAnswer > 0
-                ? FloatingActionButton(
-                  onPressed: () {
-                    setState(() {
-                      current++;
-                      selectAnswer = 0;
-                      finalAnswer = 0;
-                    });
-                  },
-                  child: const Icon(Icons.arrow_forward),
-                )
-                : null,
-      ),
+          );
+        }
+        return const Center(child: CircularProgressIndicator());
+      },
     );
   }
 }
